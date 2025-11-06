c1cefb68f845d78f5283ae2ce627a4886e61e652
Gabriel Bryan <gabriel.nacimento32@gmail.com>
2025-11-06 19:24:37 -0300

    Atualização Final

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,18 @@ function EnviarMensagem() {
         alert('Sua solicitação sobre ' + assunto + ' foi enviada com sucesso!');
     }
 }
+
+function EnviarMensagemII() {
+    const nome = document.getElementById('nome').value.trim() ? document.getElementById('nome').value.trim() : '';
+    const email = document.getElementById('email').value.trim() ? document.getElementById('email').value.trim() : '';
+    const telefone = document.getElementById('telefone').value.trim() ? document.getElementById('telefone').value.trim() : '';
+    const vaga = document.getElementById('vaga').value.trim();
+    const curriculo = document.getElementById('curriculo').value.trim();
+    const lgpd = document.getElementById('lgpd').value.trim() ? document.getElementById('lgpd').value.trim() : '';
+
+    if (!nome || !email || !telefone || !curriculo || !lgpd || !vaga) {
+        alert('Por favor, preencha todos os campos obrigatórios antes de enviar.');
+    } else {
+        alert('Sua solicitação sobre ' + vaga + ' foi enviada com sucesso!');
+    }
+}
